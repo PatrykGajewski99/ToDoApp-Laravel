ddcdb8af5d0bf863f86eff9829a141ad7a59e949
patry <patryk.gajewski@pollub.edu.pl>
2022-10-10 14:34:13 +0200

    email after crud function added

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ListFormRequest;
+use App\Mail\AddListEmail;
+use App\Mail\DeleteListEmail;
 use App\Models\Roll;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 use Mockery\Exception;
 use Illuminate\Support\Facades\DB;
@@ -26,35 +29,61 @@ class ListController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
-            'list_name' => 'required|string|min:3|max:60|unique:lists'
-        ]);
+        try {
 
-        $list=new Roll;
+            $request->validate([
+                'list_name' => 'required|string|min:3|max:60|unique:lists'
+            ]);
+            $userEmail = auth()->user()->email;
+            $list = new Roll;
 
-        $list->user_id = auth()->user()->id;
-        $list->list_name = $request->list_name;
+            $list->user_id = auth()->user()->id;
+            $list->list_name = $request->list_name;
 
-        $list->save();
+            $list->save();
+            $mailData = [
+                'title' => 'List added',
+                'body' => 'List ' . $list->list_name . ' added successfully'
+            ];
+            Mail::to($userEmail)->send(new AddListEmail($mailData));
 
-        return back()->with('success','List added successfully');
+            return back()->with('success', 'List added successfully');
+
+        } catch (\Exception $e) {
+
+            return back()->with($e->getMessage());
+        }
+
 
     }
 
     public function show(Request $request)
     {
-       $userID=auth()->user()->id;
+        try {
+            $userID = auth()->user()->id;
 
-       $lists=DB::table('lists')->where('user_id',$userID)->get();
+            $lists = DB::table('lists')->where('user_id', $userID)->get();
 
-       return view('dashboard',['lists' => $lists]);
+            return view('dashboard', ['lists' => $lists]);
+
+        } catch (\Exception $e) {
+
+            return back()->with($e->getMessage());
+        }
 
     }
 
     public function edit($id)
     {
-        $list=Roll::find($id);
-        return view('editList',compact('list'));
+        try {
+
+            $list = Roll::find($id);
+            return view('editList', compact('list'));
+
+        } catch (\Exception $e) {
+
+            return back()->with($e->getMessage());
+        }
     }
 
     public function update(Request $request, $id)
@@ -63,11 +92,11 @@ class ListController extends Controller
             $request->validate([
                 'list_name' => 'required|string|min:3|max:60|unique:lists'
             ]);
-            $newListName=$request->list_name;
+            $newListName = $request->list_name;
             Roll::find($id)->update(['list_name' => $newListName]);
-            return back()->with('success','List name changed successfully');
+            return back()->with('success', 'List name changed successfully');
 
-        }catch(Exception $e) {
+        } catch (\Exception $e) {
 
             return back()->with($e->getMessage());
         }
@@ -75,7 +104,22 @@ class ListController extends Controller
 
     public function destroy($id)
     {
-        Roll::find($id)->delete();
-        return back()->with('success','List deleted successfully');
+        try {
+            $listName = DB::table('lists')->where('id', $id)->value('list_name');;
+            Roll::find($id)->delete();
+
+            $mailData = [
+                'title' => 'List deleted',
+                'body' => 'List ' . $listName . ' deleted successfully'
+            ];
+
+            Mail::to(auth()->user()->email)->send(new DeleteListEmail($mailData));
+            return back()->with('success', 'List deleted successfully');
+
+        } catch (\Exception $e) {
+
+            return back()->with($e->getMessage());
+        }
+
     }
 }
--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\AddTaskEmail;
+use App\Mail\DeleteTaskEmail;
+use App\Mail\UpdateTaskEmail;
 use App\Models\Roll;
 use App\Models\Task;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 use Mockery\Exception;
 
 class TaskController extends Controller
@@ -18,80 +23,113 @@ class TaskController extends Controller
      */
     public function create($id)
     {
-        $listID=$id;
-        return view('addTask',compact('listID'));
+        $listID = $id;
+        return view('addTask', compact('listID'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request,$id)
-    {
-        $request->validate([
-            'task_name' => 'required|string|min:3|max:256'
-        ]);
-
-        $task=new Task;
-
-        $task->user_id = auth()->user()->id;
-        $task->list_id=$id;
-        $task->task_name = $request->task_name;
-        $task->status="to-do";
-
-        $task->save();
-
-        return back()->with('success','Task added successfully');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        $userID=auth()->user()->id;
-        $listID=$id;
-
-        $tasks=DB::table('tasks')->where([['user_id',$userID],['list_id',$listID]])->get();
-
-        return view('showTasks',['tasks' => $tasks]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        $task=Task::find($id);
-        return view('editTask',compact('task'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function store(Request $request, $id)
     {
         try {
             $request->validate([
                 'task_name' => 'required|string|min:3|max:256'
             ]);
-            $data=$request->all();
-            Task::find($id)->update($data);
-            return back()->with('success','Task changed successfully');
 
-        }catch(Exception $e) {
+            $task = new Task;
+
+            $task->user_id = auth()->user()->id;
+            $task->list_id = $id;
+            $task->task_name = $request->task_name;
+            $task->status = "to-do";
+
+            $task->save();
+
+            $mailData = [
+                'title' => 'Task added',
+                'body' => 'Task ' . $task->task_name . ' added successfully'
+            ];
+            Mail::to(auth()->user()->email)->send(new AddTaskEmail($mailData));
+
+            return back()->with('success', 'Task added successfully');
+        } catch (\Exception $e) {
+            return back()->with($e->getMessage());
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        try {
+
+            $userID = auth()->user()->id;
+            $listID = $id;
+
+            $tasks = DB::table('tasks')->where([['user_id', $userID], ['list_id', $listID]])->get();
+
+            return view('showTasks', ['tasks' => $tasks]);
+
+        } catch (\Exception $e) {
+
+            return back()->with($e->getMessage());
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        try {
+
+            $task = Task::find($id);
+            return view('editTask', compact('task'));
+
+        } catch (\Exception $e) {
+
+            return back()->with($e->getMessage());
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        try {
+
+            $request->validate([
+                'task_name' => 'required|string|min:3|max:256'
+            ]);
+            $data = $request->all();
+            $task_name = $request->input('task_name');
+            $userID = DB::table('tasks')->value('user_id');
+            $userEmail = DB::table('users')->where('id', $userID)->value('email');
+            Task::find($id)->update($data);
+            $mailData = [
+                'title' => 'Updated task',
+                'body' => 'Task ' . $task_name . ' updated successfully'
+            ];
+            Mail::to($userEmail)->send(new UpdateTaskEmail($mailData));
+            return back()->with('success', 'Task changed successfully');
+
+        } catch (\Exception $e) {
 
             return back()->with($e->getMessage());
         }
@@ -100,12 +138,29 @@ class TaskController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        Task::find($id)->delete();
-        return back()->with('success','Task deleted successfully');
+        try {
+
+            $taskName = DB::table('tasks')->where('id', $id)->value('task_name');;
+            Task::find($id)->delete();
+
+            $mailData = [
+                'title' => 'Task deleted',
+                'body' => 'Task ' . $taskName . ' deleted successfully'
+            ];
+
+            Mail::to(auth()->user()->email)->send(new DeleteTaskEmail($mailData));
+
+            return back()->with('success', 'Task deleted successfully');
+
+        } catch (\Exception $e) {
+
+            return back()->with($e->getMessage());
+        }
+
     }
 }
